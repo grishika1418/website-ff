8f42d979e3bbd532327fbfa885f3558f2a444e31
grishika1418 <grreddy1418@gmail.com>
2026-02-07 23:07:02 +0530

    prod version

--- val-site/vite.config.js
+++ val-site/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // IMPORTANT: Replace 'val-site' with your actual GitHub repository name
+  base: '/website-ff/',
 })
